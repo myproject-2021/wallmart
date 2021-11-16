5c1f9399dab16a71450f163610671f5d8666c7dc
mmsr <munnangi.msr@gmail.com>
2021-11-16 06:52:56 +0000

    no message

--- demo.sh
+++ demo.sh
@@ -1,6 +1,3 @@
-this is dev branch now
-this is the most imporent tool for git
-that was the useful in your carrer
-most of the prastic it can be a master
-that was the useful ness of the line
-that was the new commit of the file
+this was first commit
+this is an master branch
+
